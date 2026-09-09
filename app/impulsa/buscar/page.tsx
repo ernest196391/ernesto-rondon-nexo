d93d6f3e687ad1feb466a220bde43cf2031b460d
ernest196391 <ernest196391@gmail.com>
2026-09-09 13:39:57 +0000

    style: apply premium Impulsa system to search

--- app/impulsa/buscar/page.tsx
+++ app/impulsa/buscar/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { currentCommercialActor } from "../../../lib/commercial/auth";
 import NexoBuscaClient from "./NexoBuscaClient";
 import "../impulsa.css";
+import "../impulsa-premium.css";
 import "./nexo-busca.css";
 export const dynamic = "force-dynamic";
 export default async function Page() {
